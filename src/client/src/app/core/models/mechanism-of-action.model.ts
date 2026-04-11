f7c440606888c911d2dabe1b371edeaa7c845f44
Aaditya Madala <aaditya.madala@gmail.com>
2026-04-11 19:18:07 -0400

    feat(models): add MechanismOfAction and RouteOfAdministration types

---
+++ src/client/src/app/core/models/mechanism-of-action.model.ts
@@ -0,0 +1,10 @@
+export interface MechanismOfAction {
+  id: string;
+  space_id: string;
+  created_by: string | null;
+  name: string;
+  description: string | null;
+  display_order: number;
+  created_at: string;
+  updated_at: string;
+}
